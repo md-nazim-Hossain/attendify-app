6ec661204f69a2cd95a11a32649d8739f797773c
Md Nazim Hossain <86850859+md-nazim-Hossain@users.noreply.github.com>
2024-11-27 23:55:10 +0600

    fix: :zap: added empty state design

--- src/screens/protected/MyRequestScreen.tsx
+++ src/screens/protected/MyRequestScreen.tsx
@@ -1,4 +1,4 @@
-import {View} from 'react-native';
+import {Image, View} from 'react-native';
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {spacing} from '@/theme/spacing';
@@ -6,15 +6,30 @@ import {colors} from '@/theme/colors';
 import GradientButton from '@/components/button/GradientButton';
 import {useAuth} from '@/navigations/AuthProvider';
 import AddNewLeaveRequestForm from '@/components/leaveRequest/AddNewLeaveRequestForm';
-import MyRequest from '@/components/leaveRequest/MyRequest';
+import Text from '@/components/text/Text';
+import {applyOpacity} from '@/utils/applyOpacity';
+// import MyRequest from '@/components/leaveRequest/MyRequest';
 
 const MyRequestScreen = () => {
   const {isAdmin} = useAuth();
   return (
     <View style={styles.container}>
-      <View>
-        <MyRequest />
+      <Text preset="h2" style={styles.title}>
+        My requests
+      </Text>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          resizeMode="cover"
+          source={require('@/assets/images/leave-request-empty.png')}
+        />
+        <Text preset="small" style={styles.text}>
+          No requests to display
+        </Text>
       </View>
+      {/* <View>
+        <MyRequest />
+      </View> */}
       {!isAdmin && (
         <AddNewLeaveRequestForm
           trigger={({ref}) => (
@@ -38,5 +53,26 @@ const styles = StyleSheet.create({
     padding: spacing[3],
     backgroundColor: colors.white,
     gap: spacing[3],
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    height: 160,
+    width: 200,
+  },
+  text: {
+    textAlign: 'center',
+    color: applyOpacity(colors['muted-foreground'], 0.4),
+    fontWeight: 800,
+    letterSpacing: 1.3,
+    marginTop: spacing[2],
+  },
+  title: {
+    color: colors['secondary-foreground'],
+    letterSpacing: 20 * 0.08,
+    fontWeight: 900,
+    textAlign: 'center',
   },
 });
